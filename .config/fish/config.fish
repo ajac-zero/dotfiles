6824193068bcb206ab6ef02328ed8cee0ef6db0c
ajac-zero <ajcardoza2000@gmail.com>
2025-02-05 00:03:03 -0600

    fixed mise activate

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-mise activate fish | source
+~/.local/bin/mise activate fish | source
 
 zoxide init fish | source
 
